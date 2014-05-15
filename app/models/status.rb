77a43b647b81bf4a546c608a8642ea08d99f6ee9
Felicia Wang <felis_3007@yahoo.com>
2014-05-15 15:43:42 -0700

    update the unit testing

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,6 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
+
+  validates :content, length: {maximum: 150}, allow_blank: true
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a status should be no more than 150 characters" do 
+ 	status = Status.new
+ 	status.content = statuses(:one).content
+
+ 	assert !status.save
+ 	assert !status.errors[:content].empty?
+  end
 end
